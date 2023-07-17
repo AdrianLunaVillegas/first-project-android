4751c7286ad516aadfddb5a2319bae6abe94fc38
AdrianLunaVillegas <adrian.92.luna@gmail.com>
2023-07-17 22:39:33 +0200

    added the last 3 buttons

--- app/src/main/java/com/adriandevs/androidmaster1/imccalculator/ImcCalculatorActivity.kt
+++ app/src/main/java/com/adriandevs/androidmaster1/imccalculator/ImcCalculatorActivity.kt
@@ -7,6 +7,7 @@ import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
 import com.adriandevs.androidmaster1.R
 import com.adriandevs.androidmaster1.databinding.ActivityImcCalculatorBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.slider.RangeSlider
 import java.text.DecimalFormat
 
@@ -14,11 +15,20 @@ class ImcCalculatorActivity : AppCompatActivity() {
     private lateinit var binding: ActivityImcCalculatorBinding
     private var isMaleSelected: Boolean = true
     private var isFemaleSelected: Boolean = false
+    private var currentWeight: Int = 50
+    private var currentAge: Int = 30
+
 
     private lateinit var btnMale: CardView
     private lateinit var btnFemale: CardView
     private lateinit var tvHeight: TextView
     private lateinit var rsHeight: RangeSlider
+    private lateinit var btnSubtractWeight: FloatingActionButton
+    private lateinit var btnPlusWeight: FloatingActionButton
+    private lateinit var tvWeight: TextView
+    private lateinit var btnSubtractAge: FloatingActionButton
+    private lateinit var btnPlusAge: FloatingActionButton
+    private lateinit var tvAge:TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,6 +42,8 @@ class ImcCalculatorActivity : AppCompatActivity() {
         initComponents()
         initListeners()
         setGenderColor()
+        setWeight()
+        setAge()
     }
 
     private fun initComponents() {
@@ -39,6 +51,12 @@ class ImcCalculatorActivity : AppCompatActivity() {
         btnFemale = binding.btnFemale
         tvHeight = binding.tvHeight
         rsHeight = binding.rsHeight
+        btnSubtractWeight = binding.btnSubtractWeight
+        btnPlusWeight = binding.btnPlusWeight
+        tvWeight = binding.tvWeight
+        btnSubtractAge = binding.btnSubtractAge
+        btnPlusAge = binding.btnPlusAge
+        tvAge = binding.tvAge
     }
 
     private fun initListeners() {
@@ -58,6 +76,30 @@ class ImcCalculatorActivity : AppCompatActivity() {
             val result = df.format(value)
             tvHeight.text = "$result cm"
         }
+        btnPlusWeight.setOnClickListener {
+            currentWeight += 1
+            setWeight()
+        }
+        btnSubtractWeight.setOnClickListener {
+            currentWeight -= 1
+            setWeight()
+        }
+        btnPlusAge.setOnClickListener {
+            currentAge += 1
+            setAge()
+        }
+        btnSubtractAge.setOnClickListener {
+            currentAge -= 1
+            setAge()
+        }
+    }
+
+    private fun setWeight() {
+        tvWeight.text = currentWeight.toString()
+    }
+
+    private fun setAge() {
+        tvAge.text = currentAge.toString()
     }
 
     private fun setGenderColor() {
@@ -79,4 +121,5 @@ class ImcCalculatorActivity : AppCompatActivity() {
 
         return ContextCompat.getColor(this, colorReference)
     }
+
 }
