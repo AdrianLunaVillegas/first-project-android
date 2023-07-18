f46b27b7a1e4f28027f17e7cb77a041edd7f3189
AdrianLunaVillegas <adrian.92.luna@gmail.com>
2023-07-18 10:46:40 +0200

    added
    added nestedScrollView and more things

--- app/src/main/java/com/adriandevs/androidmaster1/imccalculator/ImcCalculatorActivity.kt
+++ app/src/main/java/com/adriandevs/androidmaster1/imccalculator/ImcCalculatorActivity.kt
@@ -1,14 +1,10 @@
 package com.adriandevs.androidmaster1.imccalculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
-import androidx.cardview.widget.CardView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.adriandevs.androidmaster1.R
 import com.adriandevs.androidmaster1.databinding.ActivityImcCalculatorBinding
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.slider.RangeSlider
 import java.text.DecimalFormat
 
 class ImcCalculatorActivity : AppCompatActivity() {
@@ -18,19 +14,6 @@ class ImcCalculatorActivity : AppCompatActivity() {
     private var currentWeight: Int = 50
     private var currentAge: Int = 30
 
-
-    private lateinit var btnMale: CardView
-    private lateinit var btnFemale: CardView
-    private lateinit var tvHeight: TextView
-    private lateinit var rsHeight: RangeSlider
-    private lateinit var btnSubtractWeight: FloatingActionButton
-    private lateinit var btnPlusWeight: FloatingActionButton
-    private lateinit var tvWeight: TextView
-    private lateinit var btnSubtractAge: FloatingActionButton
-    private lateinit var btnPlusAge: FloatingActionButton
-    private lateinit var tvAge:TextView
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityImcCalculatorBinding.inflate(layoutInflater)
@@ -39,67 +22,58 @@ class ImcCalculatorActivity : AppCompatActivity() {
     }
 
     private fun initialize() {
-        initComponents()
         initListeners()
         setGenderColor()
         setWeight()
         setAge()
     }
 
-    private fun initComponents() {
-        btnMale = binding.btnMale
-        btnFemale = binding.btnFemale
-        tvHeight = binding.tvHeight
-        rsHeight = binding.rsHeight
-        btnSubtractWeight = binding.btnSubtractWeight
-        btnPlusWeight = binding.btnPlusWeight
-        tvWeight = binding.tvWeight
-        btnSubtractAge = binding.btnSubtractAge
-        btnPlusAge = binding.btnPlusAge
-        tvAge = binding.tvAge
-    }
-
     private fun initListeners() {
-        binding.btnMale.setOnClickListener {
-            isMaleSelected = true
-            isFemaleSelected = false
-            setGenderColor()
-        }
+        binding.apply {
 
-        binding.btnFemale.setOnClickListener {
-            isMaleSelected = false
-            isFemaleSelected = true
-            setGenderColor()
-        }
-        rsHeight.addOnChangeListener { _, value, _ ->
-            val df = DecimalFormat("#.##")
-            val result = df.format(value)
-            tvHeight.text = "$result cm"
-        }
-        btnPlusWeight.setOnClickListener {
-            currentWeight += 1
-            setWeight()
-        }
-        btnSubtractWeight.setOnClickListener {
-            currentWeight -= 1
-            setWeight()
-        }
-        btnPlusAge.setOnClickListener {
-            currentAge += 1
-            setAge()
-        }
-        btnSubtractAge.setOnClickListener {
-            currentAge -= 1
-            setAge()
+            btnMale.setOnClickListener {
+                isMaleSelected = true
+                isFemaleSelected = false
+                setGenderColor()
+            }
+
+            btnFemale.setOnClickListener {
+                isMaleSelected = false
+                isFemaleSelected = true
+                setGenderColor()
+            }
+            rsHeight.addOnChangeListener { _, value, _ ->
+                val df = DecimalFormat("#.##")
+                val result = df.format(value)
+                val text = "$result cm"
+                tvHeight.text = text
+            }
+
+            btnPlusWeight.setOnClickListener {
+                currentWeight += 1
+                setWeight()
+            }
+            btnSubtractWeight.setOnClickListener {
+                currentWeight -= 1
+                setWeight()
+            }
+            btnPlusAge.setOnClickListener {
+                currentAge += 1
+                setAge()
+            }
+            btnSubtractAge.setOnClickListener {
+                currentAge -= 1
+                setAge()
+            }
         }
     }
 
     private fun setWeight() {
-        tvWeight.text = currentWeight.toString()
+        binding.tvWeight.text = currentWeight.toString()
     }
 
     private fun setAge() {
-        tvAge.text = currentAge.toString()
+        binding.tvAge.text = currentAge.toString()
     }
 
     private fun setGenderColor() {
@@ -107,9 +81,10 @@ class ImcCalculatorActivity : AppCompatActivity() {
         val maleBackgroundColor = getBackgroundColor(isMaleSelected)
         val femaleBackgroundColor = getBackgroundColor(isFemaleSelected)
 
-        binding.btnMale.setCardBackgroundColor(maleBackgroundColor)
-        binding.btnFemale.setCardBackgroundColor(femaleBackgroundColor)
-
+        binding.apply {
+            btnMale.setCardBackgroundColor(maleBackgroundColor)
+            btnFemale.setCardBackgroundColor(femaleBackgroundColor)
+        }
     }
 
     private fun getBackgroundColor(isSelectedComponent: Boolean): Int {
@@ -123,3 +98,5 @@ class ImcCalculatorActivity : AppCompatActivity() {
     }
 
 }
+
+
