789c443126a355e472b31d9d2e603885887297f1
AdrianLunaVillegas <adrian.92.luna@gmail.com>
2023-07-21 13:25:38 +0200

    add GenderCardView
    create a gendercardview to optimize the code

--- app/src/main/java/com/adriandevs/androidmaster1/firstapp/common/GenderCardView.kt
+++ app/src/main/java/com/adriandevs/androidmaster1/firstapp/common/GenderCardView.kt
@@ -3,33 +3,27 @@ package com.adriandevs.androidmaster1.firstapp.common
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import android.widget.ImageView
-import android.widget.TextView
 import com.adriandevs.androidmaster1.R
+import com.adriandevs.androidmaster1.databinding.CardViewGenderBinding
 import com.google.android.material.card.MaterialCardView
 
 class GenderCardView(
     context: Context, attributeSet: AttributeSet
 ) : MaterialCardView(context, attributeSet) {
-    //private lateinit var binding: MaterialCardView como trabajo con Binding?////////////////////
 
-    private val imageView: ImageView
-    private val textView: TextView
+    private val binding = CardViewGenderBinding.inflate(
+        LayoutInflater.from(context), this, true
+    )
 
     init {
-        LayoutInflater.from(context).inflate(R.layout.card_view_gender, this)
-
-        imageView = findViewById(R.id.gMale)
-        textView = findViewById((R.id.tvMale))
 
         val attrsArray = context.obtainStyledAttributes(attributeSet, R.styleable.GenderCardView)
-
-
         val customImage = attrsArray.getResourceId(R.styleable.GenderCardView_customImage, 0)
         val customText = attrsArray.getString(R.styleable.GenderCardView_customText)
-
-        imageView.setImageResource(customImage)
-        textView.text = customText
+        binding.apply {
+            gMale.setImageResource(customImage)
+            tvMale.text = customText
+        }
 
         attrsArray.recycle()
 
